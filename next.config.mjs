40ef5789d1af00de0bc8e8d76511d7820e5ca2b7
Bhavya Popat <bhavyabpopat7777@gmail.com>
2026-09-14 09:49:50 +0530

    origins fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.1.100', '192.168.1.*'],
   reactCompiler: true,
   reactStrictMode: false,
   devIndicators: false,
